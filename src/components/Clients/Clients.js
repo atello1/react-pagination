af2687ee98c6bb35053c3ac856a82218a347e74d
Ana <ana.belen.tello@outlook.com>
2019-01-03 23:26:44 -0500

    restart project for pagination

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import Client from "../Client/Client";
-import PaginationComponent from "../Pagination/Pagination";
 
 class Clients extends Component {
   constructor(props) {
@@ -11,28 +10,21 @@ class Clients extends Component {
   }
 
   componentDidMount() {
-fetch("https://api.renmark.ir/companies")
-    .then(function(response){return response.json()})
-    .then(response => {
-      this.setState({clients: response.data});
-    })
-
+    fetch("https://api.renmark.ir/companies")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(response => {
+        this.setState({ clients: response.data });
+      });
   }
   render() {
     return (
-<div className="row">
-{this.state.clients.map((client,index) =>
-  <Client
-key={index}
-    src={client.logo}
-    name={client.name}
-  />
-)}
-<PaginationComponent />
-
-</div>
-
-
+      <div className="row">
+        {this.state.clients.map((client, index) => (
+          <Client key={index} src={client.logo} name={client.name} />
+        ))}
+      </div>
     );
   }
 }
