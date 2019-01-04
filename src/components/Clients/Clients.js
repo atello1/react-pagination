b718dee4011202c5cf9f2784336ff837e4f8d137
Ana <ana.belen.tello@outlook.com>
2019-01-04 15:46:25 -0500

    working pagination

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import Client from "../Client/Client";
+import Pagination from "jw-react-pagination";
 
-class Clients extends Component {
+class exampleItems extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clients: []
+      exampleItems: [],
+      pageOfItems: []
     };
+    this.onChangePage = this.onChangePage.bind(this);
   }
 
   componentDidMount() {
@@ -15,18 +18,33 @@ class Clients extends Component {
         return response.json();
       })
       .then(response => {
-        this.setState({ clients: response.data });
+        this.setState({ exampleItems: response.data });
       });
+  }
+
+  onChangePage(pageOfItems) {
+    // update state with new page of items
+    this.setState({ pageOfItems: pageOfItems });
   }
   render() {
     return (
+<div>
       <div className="row">
-        {this.state.clients.map((client, index) => (
+        {this.state.pageOfItems.map((client, index) => (
           <Client key={index} src={client.logo} name={client.name} />
         ))}
+
       </div>
+<div className="row">
+
+        <Pagination
+          items={this.state.exampleItems}
+          onChangePage={this.onChangePage}
+        />
+</div>
+</div>
     );
   }
 }
 
-export default Clients;
+export default exampleItems;
